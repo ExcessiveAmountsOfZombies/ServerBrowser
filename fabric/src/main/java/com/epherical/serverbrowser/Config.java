5d004f16f4d1bfd3537a181e7a65dd74db43eef8
Thonk <30448663+ExcessiveAmountsOfZombies@users.noreply.github.com>
2023-02-04 16:02:36 -0600

    add config option

---
+++ fabric/src/main/java/com/epherical/serverbrowser/Config.java
@@ -0,0 +1,48 @@
+package com.epherical.serverbrowser;
+
+import com.epherical.bozo.CommonPlatform;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.stream.JsonReader;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
+public class Config {
+
+    public static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+
+    public ConfigSettings loadConfig() {
+        ConfigSettings settings = null;
+        Path path = CommonPlatform.platform.getRootConfigPath().resolve("config.json");
+
+        if (Files.exists(path)) {
+            try {
+                String json = Files.readString(path);
+                settings = GSON.fromJson(json, ConfigSettings.class);
+                return settings;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                Files.createDirectories(CommonPlatform.platform.getRootConfigPath());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            settings = new ConfigSettings();
+        }
+
+        try {
+            Files.writeString(path, GSON.toJson(settings), StandardCharsets.UTF_8, StandardOpenOption.CREATE);
+            return settings;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return settings;
+    }
+
+}
