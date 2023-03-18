62ec4710ce1cf5486f9b2e5e364a34fb92b676ff
Thonk <30448663+ExcessiveAmountsOfZombies@users.noreply.github.com>
2023-03-17 20:09:26 -0500

    Display the red circle on forge too

--- forge/src/main/java/com/epherical/serverbrowser/mixin/TitleScreenMixin.java
+++ forge/src/main/java/com/epherical/serverbrowser/mixin/TitleScreenMixin.java
@@ -1,9 +1,7 @@
 package com.epherical.serverbrowser.mixin;
 
 import com.epherical.serverbrowser.client.CommonClient;
-import com.epherical.serverbrowser.client.OfficialServerListing;
 import com.epherical.serverbrowser.client.ScreenButtonGrabber;
-import com.epherical.serverbrowser.client.screen.ServerBrowserScreen;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.BufferBuilder;
 import com.mojang.blaze3d.vertex.BufferUploader;
@@ -12,58 +10,43 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.Tesselator;
 import com.mojang.blaze3d.vertex.VertexFormat;
 import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.events.GuiEventListener;
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.client.gui.screens.multiplayer.JoinMultiplayerScreen;
-import net.minecraft.client.gui.screens.multiplayer.ServerSelectionList;
+import net.minecraft.client.gui.screens.TitleScreen;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(JoinMultiplayerScreen.class)
-public class JoinMultiplayerScreenMixin extends Screen implements ScreenButtonGrabber {
+@Mixin(TitleScreen.class)
+public abstract class TitleScreenMixin extends Screen implements ScreenButtonGrabber {
 
-
-    @Shadow protected ServerSelectionList serverSelectionList;
-
-    @Shadow private Button editButton;
-
-    @Shadow private Button deleteButton;
-
-
-    private Button serverBrowser$button;
 
     private int serverBrowser$color = 0xFFFF0000;
     private int serverBrowser$time;
 
-    protected JoinMultiplayerScreenMixin(Component component) {
+    private Button serverBrowser$grabbedMultiplayerButton;
+
+    protected TitleScreenMixin(Component component) {
         super(component);
     }
 
-    @Inject(method = "init", at = @At(value = "TAIL"))
-    public void serverBrowserAddBrowserButton(CallbackInfo ci) {
-        serverBrowser$button = this.addRenderableWidget(new Button(3, 3, 120, 20, Component.translatable("Server Browser"), button -> {
-            minecraft.setScreen(new ServerBrowserScreen(this));
-            CommonClient.getInstance().getSettings().serverBrowserNotification = false;
-            CommonClient.getInstance().saveConfig();
-        }));
+    @Redirect(method = "createNormalMenuOptions", at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/client/gui/screens/TitleScreen;addRenderableWidget(Lnet/minecraft/client/gui/components/events/GuiEventListener;)Lnet/minecraft/client/gui/components/events/GuiEventListener;", ordinal = 1))
+    public GuiEventListener serverBrowser$findMultiplayerButton(TitleScreen instance, GuiEventListener guiEventListener) {
+        serverBrowser$grabbedMultiplayerButton = (Button) guiEventListener;
+        addRenderableWidget(serverBrowser$grabbedMultiplayerButton);
+        return guiEventListener;
     }
 
-    @Inject(method = "onSelectedChange", at = @At("TAIL"))
-    public void serverBrowserPreventChange(CallbackInfo ci) {
-        if (this.serverSelectionList.getSelected() instanceof OfficialServerListing.OfficialEntry) {
-            this.editButton.active = false;
-            this.deleteButton.active = false;
-        }
-    }
 
     @Inject(method = "render", at = @At("TAIL"))
-    public void serverBrowser$modifyRender(PoseStack poseStack, int mouseX, int mouseY, float partialTick, CallbackInfo ci) {
-        int yPos = serverBrowser$button.y;
-        int xPos = serverBrowser$button.x;
+    public void serverBrowser$renderOutline(PoseStack poseStack, int mouseX, int mouseY, float partialTick, CallbackInfo ci) {
+        int yPos = serverBrowser$grabbedMultiplayerButton.y;
+        int xPos = serverBrowser$grabbedMultiplayerButton.x;
 
         if (CommonClient.displayCircle()) {
             if (serverBrowser$time < 20) {
@@ -96,6 +79,13 @@ public class JoinMultiplayerScreenMixin extends Screen implements ScreenButtonGr
                 double sin = Math.sin(-(j * twoPI / 360));
                 double cos = Math.cos(-(j * twoPI / 360));
 
+                /*if (j >= (progress + 90)) {
+                    bufferBuilder.vertex(centerX + (innerRad * cos), centerY + (innerRad * sin), getBlitOffset()).color(56, 56, 56, 255).endVertex();
+                    bufferBuilder.vertex(centerX + (outerRad * cos), centerY + (outerRad * sin), getBlitOffset()).color(56, 56, 56, 255).endVertex();
+                } else {
+
+                }*/
+
                 bufferBuilder.vertex(centerX + (innerRad * cos), centerY + (innerRad * sin), getBlitOffset()).color(serverBrowser$color).endVertex();
                 bufferBuilder.vertex(centerX + (outerRad * cos), centerY + (outerRad * sin), getBlitOffset()).color(serverBrowser$color).endVertex();
             }
@@ -108,6 +98,6 @@ public class JoinMultiplayerScreenMixin extends Screen implements ScreenButtonGr
 
     @Override
     public Button grabbedButton() {
-        return serverBrowser$button;
+        return serverBrowser$grabbedMultiplayerButton;
     }
 }
